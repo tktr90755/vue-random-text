a832ca4da326006a8b515e4a1f17ee6a74cfb3cd
Toru Takatori <i@218.jp>
2019-07-08 19:05:57 +0900

    [update]微修正

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <RandomText text='Test test'/>
-    <RandomText text='Apple redirects here. For other companies of similar name' kind='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'/>
+    <RandomText text='Test test'/></br>
+    <RandomText text='Apple redirects here. For other companies of similar name' kind='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'/></br>
     <RandomText text='春はあけぼの。やうやう白くなりゆく山際、少し明かりて、紫だちたる雲の細くたなびきたる。 ' kind='亜唖娃阿哀愛挨姶逢葵茜穐悪握渥旭葦芦鯵梓圧斡扱宛姐虻飴絢綾鮎或粟袷安庵按暗案闇鞍杏以伊位依偉囲夷委威尉惟意慰易椅為畏異移維緯胃萎衣謂違遺医井亥域育郁磯一壱溢逸稲茨芋鰯允印咽員因姻引飲淫胤蔭'/>
   </div>
 </template>
